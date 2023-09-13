2c71122f8130f3e4f2595d68df3e896fcf7ca9e4
kepri-dev <141520735+kepri-dev@users.noreply.github.com>
2023-09-13 14:57:25 +0100

    main.js

--- main.js
+++ main.js
@@ -1,37 +1,51 @@
 let isNewCalculus = false;
 let previousResult = "";
+let memory = 0;
 
 function ipermax(event) {
-  event.preventDefault();
-  const clickedButton = event.target;
-  const buttonValue = clickedButton.innerText || clickedButton.textContent;
-  const output = document.getElementById("result");
-  const theButton = document.getElementById("button_clicked");
+  event.preventDefault(); // Prevents the default action of the button (like form submission).
+  const clickedButton = event.target; // References the clicked button.
+  const buttonValue = clickedButton.innerText || clickedButton.textContent; // Get the text inside the clicked button.
+  const output = document.getElementById("result"); // Target the result textarea.
+  const theButton = document.getElementById("button_clicked"); // Target the last clicked button textarea.
 
   if (buttonValue == "=") {
-    output.value = eval(output.value);
-    theButton.value = output.value;
-    previousResult = output.value;
+    output.value = eval(output.value); // Evaluate the mathematical expression in the result textarea.
+    theButton.value = output.value; // Update the last clicked button textarea with the result.
+    previousResult = output.value; // Store the current result for future use.
     isNewCalculus = true;
   } else if (buttonValue == "AC") {
-    output.value = "";
-    theButton.value = "";
+    output.value = ""; // Clear the result textarea.
+    theButton.value = ""; // Clear the last clicked button textarea.
+  } else if (buttonValue == "M+") {
+    memory += output.value; // store the current result for future use,
+  } else if (buttonValue == "M-") {
+    memory -= output.value; // store the current result, multiplied by -1, for future use,
+  } else if (buttonValue == "MR") {
+    output.value = output.value + memory.toString(); // recall the last stored value,
+  } else if (buttonValue == "MC") {
+    memory = 0; // delete stored value from the memory.
   } else {
     if (isNewCalculus) {
-      if (
-        buttonValue == "+" ||
-        buttonValue == "-" ||
-        buttonValue == "*" ||
-        buttonValue == "/"
-      ) {
-        output.value = previousResult + buttonValue;
-      } else {
-        output.value = buttonValue;
-      }
-      isNewCalculus = false;
+      // If a result was just computed.
+      output.value = thisIsANewCalculus(buttonValue);
+      isNewCalculus = false; // Reset the new calculation flag.
     } else {
-      output.value += buttonValue;
+      output.value += buttonValue; // Append the clicked value to the current expression.
     }
-    theButton.value = buttonValue;
+    theButton.value = buttonValue; // Update the last clicked button textarea with the clicked value.
+  }
+}
+
+function thisIsANewCalculus(buttonValue) {
+  if (
+    buttonValue == "+" ||
+    buttonValue == "-" ||
+    buttonValue == "*" ||
+    buttonValue == "/"
+  ) {
+    return previousResult + buttonValue; // Use the previous result and append the clicked operation.
+  } else {
+    return buttonValue; // Otherwise, start a new calculation with the clicked number.
   }
 }
